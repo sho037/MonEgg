dacf069e0fabe4b456518773df4fe30acdfcdf5b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-04-24 11:12:12 +0900

    chore: .envファイルがなければシェルスクリプトで，コピーと app_key の生成をするように変更

--- backend/bin/start.sh
+++ backend/bin/start.sh
@@ -5,4 +5,10 @@ echo "cd /app/app" >> ~/.bashrc
 cd /app/app
 
 composer install
+
+if [ ! -e .env ]; then
+  cp .env.example .env
+  php artisan key:generate
+fi
+
 php artisan serve --host=0.0.0.0
