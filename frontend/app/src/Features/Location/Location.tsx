b371a96da4c4fdc71a5f81b17cbaee02bf89e2b0
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-27 09:56:12 +0900

    ✨ 地域を設定するUIを作成

---
+++ frontend/app/src/Features/Location/Location.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import LocationCode from "./LocationCode.json";
+import { Typography, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+
+const Location = () => {
+    const [locationCode, setLocationCode] = React.useState("");
+    React.useEffect(() => {
+        const userLocationCode = localStorage.getItem("userLocationCode");
+        if (userLocationCode) {
+            setLocationCode(userLocationCode);
+        }
+    }, []);
+
+    return (
+        <>
+            <Typography variant="h5">Location</Typography>
+            <hr />
+            <FormControl fullWidth>
+                <InputLabel id="locationSelect">Location</InputLabel>
+                <Select
+                    labelId="locationSelect"
+                    id="location"
+                    label="Location"
+                    defaultValue=""
+                    value={locationCode}
+                    onChange={(e) => {
+                        setLocationCode(e.target.value as string);
+                        localStorage.setItem("userLocationCode", e.target.value as string);
+                    }}
+                >
+                    {LocationCode.map((item) => (
+                        <MenuItem key={item.spotId} value={item.spotId}>{item.spotName}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            {locationCode === "" && (
+                <Typography variant="h6" color="error">Locationを選択してください。</Typography>
+            )}
+        </>
+    );
+};
+
+export default Location;
