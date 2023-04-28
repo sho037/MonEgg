0fbe10a71eb1da427a21ee8df8ed58ed4e4b75cb
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-29 06:04:15 +0900

    ✨ サインインするためのページを作成

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -8,7 +8,7 @@ import {
 import "./index.css";
 import {Header} from './Components/Header/Header'
 import App from "./App";
-import SignIn from "./Features/SignIn/SignIn";
+import SignInPage from "./Pages/SignInPage";
 import SignUpPage from "./Pages/SignUpPage";
 import NotFound from "./Components/NotFound/NotFound";
 
@@ -22,7 +22,7 @@ if (rootElement) {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
