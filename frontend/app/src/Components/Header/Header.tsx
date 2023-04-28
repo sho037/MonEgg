e3d0cf7875ea71fae26b98022f40e906e879d4a8
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-29 06:10:50 +0900

    💄 ボタンを動的に変更

--- frontend/app/src/Components/Header/Header.tsx
+++ frontend/app/src/Components/Header/Header.tsx
@@ -3,6 +3,35 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import axios from "axios";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+const ButtonForUser = () => {
+  axios
+    .get("http://localhost:8000/api/user/", {
+      headers: {
+        "Content-type": "Application/json",
+        Accept: "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+    })
+    .then((res) => {
+      return <AccountCircleIcon sx={{ ml: "auto" }} />;
+    })
+    .catch((err) => {
+      return (
+        <Button color="inherit" href="/signup" sx={{ ml: "auto" }}>
+          SignUp
+        </Button>
+      );
+    });
+
+  return (
+    <Button color="inherit" href="/signup" sx={{ ml: "auto" }}>
+      SignUp
+    </Button>
+  );
+};
 
 /**
  * ヘッダを表示する
@@ -17,7 +46,7 @@ export const Header = () => {
         <Typography variant="h4" component="div">
           Logpose
         </Typography>
-        <Button color="inherit" sx={{ml:"auto"}}>SignUp</Button>
+        <ButtonForUser />
       </Toolbar>
     </AppBar>
   );
