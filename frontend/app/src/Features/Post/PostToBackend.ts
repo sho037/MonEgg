b13bfcf5daae201c31085d5ee211df352f53fb8a
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-29 00:29:31 +0900

    🚧 ユーザーごとのデータが入るように

--- frontend/app/src/Features/Post/PostToBackend.ts
+++ frontend/app/src/Features/Post/PostToBackend.ts
@@ -1,41 +1,64 @@
+import React from "react";
 import axios from "axios";
 
-const PostToBackend = async (postText:string) => {
-    const locationCode = localStorage.getItem("userLocationCode");
+const Axios = axios.create({
+  baseURL: "http://localhost:8000/",
+  withCredentials: true,
+  headers: {
+    "Content-type": "Application/json",
+    Accept: "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+  },
+});
 
-    console.log("postText: " + postText);
+const api = axios.create({
+  baseURL: "http://localhost:8000/api",
+  withCredentials: true,
+  headers: {
+    "Content-type": "Application/json",
+    Accept: "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+  },
+});
 
-    const Axios = axios.create({
-        baseURL: "http://localhost:8000/",
-        withCredentials: true,
-        headers: {
-          "Content-type": "Application/json",
-          Accept: "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-        },
-      });
-
-    const api = axios.create({  
-        baseURL: "http://localhost:8000/api",
-        withCredentials: true,
-        headers: {
-          "Content-type": "Application/json",
-          "Accept": "application/json",
-          "X-Requested-With": "XMLHttpRequest"
-        }
-      });
-
-      await Axios.get("/sanctum/csrf-cookie");
-
-    // TODO ユーザーがログインしていない場合の処理を追加する
-
-    const response = await api.post("/post/store", {
-        'spot_id': locationCode,
-        'user_id': 1,
-        'content': postText,
+const user = async () => {
+  let userId;
+  await api
+    .get("/user")
+    .then((res) => {
+      console.log(res.data);
+      console.log(res.data.data);
+      console.log(res.data.data.name);
+      userId = res.data.id;
+    })
+    .catch((error) => {
+      console.log(error.data);
+      alert("サインインしてください");
+      window.location.href = "/signin";
     });
 
-    console.log(response.data);
+  return userId;
+};
+
+const PostToBackend = async (postText: string) => {
+  const locationCode = localStorage.getItem("userLocationCode");
+  let userId;
+
+  console.log("PostToBackend locationCode: " + locationCode);
+
+  console.log("PostToBackend postText: " + postText);
+
+  await Axios.get("/sanctum/csrf-cookie");
+
+  userId = await user();
+
+  const response = await api.post("/post/store", {
+    spot_id: locationCode,
+    user_id: userId,
+    content: postText,
+  });
+
+  console.log(response.data);
 };
 
 export default PostToBackend;
