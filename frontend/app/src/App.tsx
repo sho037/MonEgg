1275f2847c1495075c816130b74a053271320bea
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-29 06:25:50 +0900

    💄 Location UIを変更

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,9 +1,17 @@
 import Location from "./Features/Location/Location";
+import Post from "./Features/Post/Post";
+
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Location />
+      <Box
+        borderRadius={16}
+        sx={{ m: "auto", maxWidth: "100%", width: 500, pt: 5 }}
+      >
+        <Location />
+      </Box>
     </>
   );
 }
