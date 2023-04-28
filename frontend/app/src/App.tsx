3ccb47932249f7ea37e38f110fcfa2b7ab434313
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-29 06:57:20 +0900

    ✨ 投稿できるようにボタンを追加

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,7 +1,10 @@
 import Location from "./Features/Location/Location";
 import { Posts } from "./Features/Posts/Posts";
+import Post from "./Features/Post/Post";
 
 import { Box } from "@mui/material";
+
+const action = { icon: <Post />, name: "投稿" };
 
 function App() {
   return (
@@ -14,8 +17,18 @@ function App() {
       </Box>
       <br />
       <hr />
-      <Box sx={{p:3}}>
+      <Box sx={{ p: 3 }}>
         <Posts />
+      </Box>
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: "16px",
+          right: "16px",
+          zIndex: 9999,
+        }}
+      >
+        <Post />
       </Box>
     </>
   );
