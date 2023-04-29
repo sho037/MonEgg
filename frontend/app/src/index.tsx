4e8d2eeb3ddc1d9c16ff242f467df37374c74a4e
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-29 11:05:40 +0900

    🎨 フォーマット

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import * as ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-import {Header} from './Components/Header/Header'
+import { Header } from "./Components/Header/Header";
 import App from "./App";
 import SignInPage from "./Pages/SignInPage";
 import SignUpPage from "./Pages/SignUpPage";
@@ -16,16 +12,16 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-  <React.StrictMode>
-    <Router>
-    <Header />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
-);
-  }
+    <React.StrictMode>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  );
+}
